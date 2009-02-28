410e9d6b8d3f4c346d6b6c3aeb935d5f5825e29d
Ritesh <itsmeritesh@gmail.com>
2009-02-28 20:33:18 +0530

    adding changes to client.js

--- ritesh/demo/js/client.js
+++ ritesh/demo/js/client.js
@@ -4,9 +4,24 @@
   purpose: contains calls for synchronization of the workflow and handshakes   */
 
 
+
+function getStatus()
+ {
+  $.post("status", handleStatusResponse,"text");
+ }
+ 
+ 
 function handleStatusResponse(data)
  {
-	alert(data);
+	if(data.find("uninitialized")!= -1)
+		{
+		 setTimeout("getStatus()",2000);
+		 alert('sending');
+		}
+	else
+	   {
+	    alert( "still to be implemented");
+	   }
  }
 
 
