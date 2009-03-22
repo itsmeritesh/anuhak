7f128921f3d9c11afb3cbf08f5a0754e5048a66c
Ashwin <ashwinrajiv@gmail.com>
2009-03-22 08:01:37 +0530

    reducemanager changed to abuse cache

--- ashwin/reducemanager.py
+++ ashwin/reducemanager.py
@@ -6,40 +6,54 @@ class reducequeue:
     
     count = 0
     reduce_cache = None
-    getcount =0
+    reducelist = []
 
     def __init__(self):
         self.reduce_cache = cache()
+        self.reduce_cache.put("resultlist",self.reducelist)
     
     def put(self,id,resultdata):
         result = resultlet()
         result.token = id
         result.data = resultdata
-        self.count = self.count+1
-        self.reduce_cache.put("result"+str(self.count),result)
+        self.reducelist = self.reduce_cache.get("resultlist")
+        self.reducelist.append(result)
+        self.reduce_cache.replace("resultlist",self.reducelist)
     
         
 
 class reducemanager:
     
-    getcount = None
+
+    mycache = None
     
-    def __init__(self):
-        self.getcount = 1
-    
+    def __init__(self,number):
+        self.mycache = cache()
+
     
     def getreduceitems(self,number):
         returnlist = []
-        count = 0
-        for i in range(1,number):
-            item=self.reduce_cache.get("result"+str(self.getcount+(i-1)))
-            if item == None:
-            #reduce pairs done
-                return -1
-            returnlist.append(item)
+        templist=self.mycache.get("resultlist")
+        if len(templist) == 0:
+            self.mycache.delete("resultlist")
+            returnlist = None
+            return returnlist
+            #list is empty
             
-        self.getcount = self.getcount + number
+        for i in range(1,number):
+            try:
+                returnlist.append(templist.pop(0))
+            except:
+                #maybe we are done with the list
+                if len(returnlist) == 0:
+                    #now we have an empty list
+                    returnlist = None
+                    self.mycache.delete("resultlist")
+                    return returnlist
+
+        self.mycache.replace("resultlist",templist)
         return returnlist
+        
         
         
     
